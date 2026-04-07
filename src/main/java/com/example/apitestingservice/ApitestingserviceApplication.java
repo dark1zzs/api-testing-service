8727e094ce154cafca0beeb36e0d99c4e45d4865
Sofia <dark1@MacBook-Air-Sofia.local>
2026-04-07 22:14:59 +0300

    Initial Spring Boot project

---
+++ src/main/java/com/example/apitestingservice/ApitestingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.apitestingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitestingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitestingserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apitestingservice/ApitestingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apitestingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitestingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
